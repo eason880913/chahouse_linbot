7da7655d6aae7031be2a73119b2f549bcdaa7f2e
李一宸 <eason88091372104@gmail.com>
2021-05-03 13:58:13 +0800

    ok

--- try.py
+++
@@ -1,5 +0,0 @@
-import random, string
-
-poolOfChars  = string.ascii_letters + string.digits
-random_codes = lambda x, y: ''.join([random.choice(x) for i in range(y)])
-print (random_codes(poolOfChars, 15))
